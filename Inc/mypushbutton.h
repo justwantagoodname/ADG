85fe63c385c2672017b05026878463b7bc1037a4
SamBillon <89698096+SamBillon@users.noreply.github.com>
2022-04-13 20:33:30 +0800

    create button

---
+++ Inc/mypushbutton.h
@@ -0,0 +1,28 @@
+//
+// Created by SamChen on 2022/4/13.
+//
+
+#ifndef MYPUSHBUTTON_H
+#define MYPUSHBUTTON_H
+
+#include <QObject>
+#include<QPushButton>
+#include<QString>
+#include<QPropertyAnimation>
+
+class MyPushButton : public QPushButton {
+Q_OBJECT
+public:
+    MyPushButton(QString normalImg);
+
+    QString normalImgPath;
+
+    void zoom1();
+
+    void zoom2();
+
+signals:
+
+};
+
+#endif // MYPUSHBUTTON_H
