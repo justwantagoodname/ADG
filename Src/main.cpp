604276d046d900911c7686f5f4df326b3b53336c
Null NaN <nullnan@users.noreply.github.com>
2022-04-13 19:59:51 +0800

    delete default code

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,9 +2,7 @@
 #include <QPushButton>
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
+    QApplication app(argc, argv);
+
     return QApplication::exec();
 }
