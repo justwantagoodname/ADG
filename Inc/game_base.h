82c9537e15f9061bf2c6255c03da96e39caa09fb
Null NaN <nullnan@users.noreply.github.com>
2022-04-13 19:56:01 +0800

    add gamebase.c file

---
+++ Inc/game_base.h
@@ -0,0 +1,8 @@
+//
+// Created by null on 2022/4/13.
+//
+
+#ifndef AGD_GAME_BASE_H
+#define AGD_GAME_BASE_H
+
+#endif //AGD_GAME_BASE_H
---
+++ Src/game_base.cpp
@@ -0,0 +1,5 @@
+//
+// Created by null on 2022/4/13.
+//
+
+#include "../Inc/game_base.h"
