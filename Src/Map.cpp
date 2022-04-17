59fc68c7dafe5e5d6cf1abd8203820891c5fd2c3
Null NaN <nullnan@users.noreply.github.com>
2022-04-17 11:19:55 +0800

    try change unkonwn into map reader and create map file

--- Src/Map.cpp
+++ Src/Map.cpp
@@ -16,6 +16,16 @@ Map::~Map() noexcept {
 void Map::Map_Init(Unknown *unknown) {
    auto objects = map->getConfig("data").toObject();
    auto unknown_map = objects.value("unknown").toArray();
-
+   QVector<QVector<int> > v2;
+   QVector<int> v;
+   for (const auto &item : unknown_map) {
+//       qDebug() << item;
+       for (const auto &data : item.toArray()) {
+           v.push_back(data.toInt());
+       }
+       v2.push_back(v);
+       v.clear();
+   }
+   unknown->m.insert(1, v2);
 }
 
