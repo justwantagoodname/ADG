cc723d2b13d31f2b1125e0c59ba60f2b8144d153
Null NaN <nullnan@users.noreply.github.com>
2022-04-17 10:05:20 +0800

    add Config reader

---
+++ Src/Map.cpp
@@ -0,0 +1,5 @@
+//
+// Created by null on 22-4-17.
+//
+
+#include "Map.h"
---
+++ Src/Map.h
@@ -0,0 +1,10 @@
+//
+// Created by null on 22-4-17.
+//
+
+#ifndef AGD_MAP_H
+#define AGD_MAP_H
+class Map {
+
+};
+#endif //AGD_MAP_H
